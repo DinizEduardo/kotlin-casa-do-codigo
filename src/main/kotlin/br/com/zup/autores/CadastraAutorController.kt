bb85ba301b276fb5943865bf0e07a7b1f064ee03
DinizEduardo <eduardo.diniz987@gmail.com>
2021-03-25 14:14:28 -0300

    Validando campos da requisição

--- src/main/kotlin/br/com/zup/autores/CadastraAutorController.kt
+++ src/main/kotlin/br/com/zup/autores/CadastraAutorController.kt
@@ -3,12 +3,15 @@ package br.com.zup.autores
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.validation.Validated
+import javax.validation.Valid
 
+@Validated
 @Controller("/autores")
 class CadastraAutorController {
 
     @Post
-    fun cadastra(@Body request: CadastraAutorRequest) {
+    fun cadastra(@Body @Valid request: CadastraAutorRequest) {
         println(request)
     }
 
